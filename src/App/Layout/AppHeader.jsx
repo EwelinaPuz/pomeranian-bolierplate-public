15ba3ef9f739d5ed2c5c3104d164a5fcc2410642
EwelinaPuz <144039570+EwelinaPuz@users.noreply.github.com>
2023-11-07 15:28:55 +0100

    feat: change colors, add new icon (#1)

--- src/App/Layout/AppHeader.jsx
+++ src/App/Layout/AppHeader.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import './styles/header.css';
 
 // 1. Import SVG as React component
-import { ReactComponent as Logo } from '../Images/start-it-logo.svg';
+import { ReactComponent as PomeranianLogo } from '../Images/start-it-logo.svg';
 
 // 2. Import SVG as URL
 // import logo from '../Images/start-it-logo.svg';
@@ -11,12 +11,11 @@ import { ReactComponent as Logo } from '../Images/start-it-logo.svg';
 export function AppHeader() {
   return (
     <header>
-      <div>
         {/* 1. sposób */}
-        <Logo />
+        <PomeranianLogo />
         {/* 1. sposób */}
         {/* <img src={logo} alt="Logo" /> */}
-      </div>
+      
     </header>
   );
 }
